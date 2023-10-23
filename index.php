537c242a6a3332b64973861d246aeb04c78aeb49
Fedordv <psgameeasp@gmail.com>
2023-10-23 10:52:19 +0300

    php lesson13 wd 12

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <?php
-echo 2+2;
-$age = 25;
-$name = "John";
+$age = 19;
+$name = "Fedor";
 echo "Меня зовут " . $name . " и мне " . $age . " лет.";
+$myGlobalVariable = 8; 
+define("NUMBER_OF_DATA_TYPES", 8);
+$myAge = 19;
+$myName = "Fedor";
+echo "Всем привет, меня зовут $myName и мне $myAge лет.";
+$boolVar = true;
+$intVar = 10;
+$floatVar = 3.14;
+$arrayVar = array("apple", "banana", "cherry");
+$nullVar = null;
+$stringVar = "Hello, World!";
+echo $intVar;
+echo $floatVar;
 ?>
